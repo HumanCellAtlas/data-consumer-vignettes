2f85488c53d0fc3759478062cb60051a75a3bfb0
Ubuntu <ubuntu@ip-172-31-31-20.us-west-2.compute.internal>
2018-07-04 00:02:27 +0000

    Update query and output_format

--- tasks/Find Cell Type Count/find-cell-type-count.py
+++ tasks/Find Cell Type Count/find-cell-type-count.py
@@ -2,26 +2,58 @@
 
 import hca, json
 
+
 def count_cell_types(cell_type):
     count = 0
-    query = { "query": { "match": { "bundle.creator_uid": 0 } } }
+    query = {
+        "query" : {
+            "bool" : {
+                "must" : [{
+                    "match" : {
+                        "metadata.files.biomaterial_json.biomaterials.context.organ.text" : "spleen"
+                    }
+                }, {
+                    "match" : {
+                        "metadata.files.biomaterial_json.schema_version" : "5.1.0"
+                    }
+                }, {
+                    "range" : {
+                        "metadata.files.biomaterial_json.biomaterials.content.total_estimated_cells" : {
+                            "gt" : 0
+                        }
+                    }
+                }]
+            }
+        }
+    }
+    
+#    query = { "query" : { "bool" : { "must" : { "match" : { "*" : "*" } } } } }
+    
+#    query = { "query" : { "match" : { "search_score" : 1.0 } } }
+    
+    query = { "match" : { "search_score" : 1.0 } }
     
     # Iterate through all search results containing the chosen cell type
-    for result in hca.dss.DSSClient().post_search.iterate(es_query={}, replica='aws', output_format='raw'):
-#        print(result)
-#        bundle_uuid = result['bundle_fqid'][:36]
-#        bundle_dict = hca.dss.DSSClient().get_bundle(uuid=bundle_uuid, replica='aws')
-#        print()
-        print(json.dumps(result, indent=4, sort_keys=True))
-#        print()
-#        print(json.dumps(hca.dss.DSSClient().get_file(uuid=bundle_dict.get('bundle').get('files')[0].get('uuid'), replica='aws'), indent=4, sort_keys=True))
+    for bundle_dict in hca.dss.DSSClient().post_search.iterate(es_query=query, replica='aws', output_format='raw'):
+        
+        # Check if the bundle is missing information. If so, skip to next iteration
+        if not bundle_dict['metadata'].get('files'):
+            continue
+        
+        print(json.dumps(bundle_dict, indent=4, sort_keys=True))
+        print()
+        
+        # If the checks passed, then increment the count by the number of cells found in this file
+        count += bundle_dict['metadata']['files']['biomaterial_json']['biomaterials'][0]['content']['total_estimated_cells']
+        print(count)
         break
         
-        # Add up the total number of cells in the sample
-#        count += bundle.get('biomaterials')[0].get('content').get('total_estimated_cells')
     return count
 
-def main():
-    print(count_cell_types('spleen'))
 
+def main():
+    organ_type = 'spleen'
+    print('{} cell count: {}'.format(organ_type, count_cell_types(organ_type)))
+
+    
 main()
