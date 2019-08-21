a436b8b1490da07d180da653b5ca6ba0b0317a41
Jeffrey <wujeffrey567@gmail.com>
2019-08-21 13:14:43 -0700

    Cleaned up script to make it readable

--- tocgen.py
+++ tocgen.py
@@ -1,20 +1,29 @@
 from jinja2 import Environment, FileSystemLoader
-import os, re
+import os, glob
 
+# Sets template to toc.md.j2 located at the root of project
 env = Environment(loader=FileSystemLoader(os.path.dirname('./')))
 template = env.get_template('toc.md.j2')
 
+# Sets dir to root of project
 rootDir = '.'
 
-list_dirs = ""
-for dirName, subdirList, fileList in os.walk(rootDir):
-    if re.match('^./.git',dirName):
-        pass
-    else:
-        for fname in fileList:
-            if fname == "README.md" and dirName != ".":
-                list_dirs += "* [{}](#{})\n".format(dirName,os.path.join(dirName,fname).replace(" ", "%"))
+# Gets a list of dirs with README
+list_dirs = glob.glob("./**/*.md")
 
-vars = {"toc": list_dirs.replace("./","")}
+content = ""
+
+# For list of items, perform split to get the wanted parts of the path and append it to 
+# content var
+for items in list_dirs:
+    root, dirName, readMe = items.split("/")
+    link = os.path.join(dirName, readMe).replace(" ","%")
+    if dirName != 'test':
+        content += "[{}]({})\n".format(dirName, link)
+
+# Saves content to object that will be used to write to template 
+vars = {"toc": content}
+
+# Writes to toc.md based on template 
 with open('toc.md', 'w') as f:
     f.write(template.render(**vars))
