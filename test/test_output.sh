58db7d1c8a721e95e2b528f51eb5411fc316a8d6
Natan Lao <natanlao@users.noreply.github.com>
2019-12-17 18:34:50 -0800

    General test harness improvements (#77)
    * Use treon to run notebooks in their own directory
    
    treon==0.1.3 runs notebooks in their directory so we can discard the
    workarounds that we have
    
    * Fix output testing failing silently
    
    * Use new notebook exclusion in treon==0.1.3
    
    This means that we can throw away the workaround we had in `.travis.yml`.
    Not a super big change, but it feels nice.
    
    * Cache pip for Travis builds
    
    * Use 3.8 over 3.8-dev for Travis builds
    
    Matplotlib is choking on something that it didn't choke on before.
    Maybe this will fix it.
    
    * Temporarily disable Python 3.8 testing
    
    See #88. Waiting on Python 3.8 support in numba/llvmlite.
    
    * Fix installing requirements.txt failing silently

--- test/test_output.sh
+++ test/test_output.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 # test/test_output.sh 
-test $(grep -oc '"outputs": ' "$1") -eq $(grep -oc '"cell_type": "code"' "$1")
-if [ $? -gt 0 ]; then
-    echo "FAIL: Was output committed with this notebook? $1"
-fi
-exit 0
+echo $1
+test $(grep -oc '"outputs": ' "$1") -eq $(grep -oc '"cell_type": "code"' "$1") || exit 1
