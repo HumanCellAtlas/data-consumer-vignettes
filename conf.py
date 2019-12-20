1f4ba9931fb4969ab305e7eec2a8ab2547ead26e
Charles Reid <chmreid@ucsc.edu>
2019-12-19 16:05:50 -0800

    update sphinx ignore list

--- conf.py
+++ conf.py
@@ -38,7 +38,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
-    'vp', '_src', '_build', 'Thumbs.db', '.DS_Store'
+    'vp', 'dcv', '_src', '_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints'
 ]
 
 
